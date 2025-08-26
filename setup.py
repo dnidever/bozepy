6392909acb59fa1ba8b0c7119f6799ec6b753b19
David Nidever <dnidever@montana.edu>
2025-08-26 12:43:43 -0600

    bumped version from 1.0.13 to 1.0.14

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 #from distutils.core import setup
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, find_namespace_packages
 
 setup(name='bozepy',
-      version='1.0.13',
+      version='1.0.14',
       description='Data reduction software for MSU astronomical data',
       author='David Nidever',
       author_email='dnidever@montana.edu',
